99ed02e3780e78176a3687a561dce1fcf88fec75
lucaspere <lucasrupestre@hotmail.com>
2022-08-17 11:20:32 -0300

    finish the lab-1

--- index.js
+++ index.js
@@ -1,49 +1,97 @@
 import { GraphQLClient, gql } from 'graphql-request'
 
-const query = gql`{
-  search(query: "stars:>100", type: REPOSITORY, first: 10) {
-    nodes {
-      ... on Repository {
-         name
-        createdAt
-        updatedAt
-        pullRequests(states: MERGED) {
-          totalCount
-        }
-        issues(states: CLOSED) {
-          totalCount
-        }
-        releases {
-          totalCount
-        }
-        languages(orderBy: {field: SIZE, direction: ASC}, last: 10) {
-          nodes {
-            name
-          }
-        }issues {
-          totalCount
-        }
-        primaryLanguage {
-          name
-        }
-        updatedAt
-        releases {
-          totalCount
-        }
-        pullRequests {
-          totalCount
-        }
-        createdAt
-        nameWithOwner
-      }
-    }
-  }
-}
-`
 const client = new GraphQLClient('https://api.github.com/graphql', {
-    headers: {
-        authorization: "bearer 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",
-    },
+  headers: {
+    authorization: "bearer SEU_TOKEN_AQUI",
+  },
 })
-const data = await client.request(query, {})
-console.log(data.search.nodes)
+
+const cursores = ["", "Y3Vyc29yOjIw", "Y3Vyc29yOjQw", "Y3Vyc29yOjYw", "Y3Vyc29yOjgw"]
+const promises = []
+
+for (let i = 0; i < cursores.length; i++) {
+  let query
+  if (i === 0) {
+    query = gql`{
+      search(
+        query: "stars:>100"
+        type: REPOSITORY
+        first: 20
+      ) {
+          edges {
+            cursor
+          }
+          nodes {
+            ... on Repository {
+              name
+              createdAt
+              updatedAt
+              pullRequests(states: MERGED) {
+                totalCount
+              }
+              issuesclosed: issues(states: CLOSED) {
+                totalCount
+              }
+              issuesopen: issues(states: OPEN) {
+                totalCount
+              }
+              releases {
+                totalCount
+              }
+              primaryLanguage {
+                name
+              }
+            }
+          }
+          pageInfo {
+            endCursor
+          }
+        }
+      }`
+  } else {
+    query = gql`{
+      search(
+        query: "stars:>100"
+        type: REPOSITORY
+        first: 20
+        after: "${cursores[i]}"
+      ) {
+          edges {
+            cursor
+          }
+          nodes {
+            ... on Repository {
+              name
+              createdAt
+              updatedAt
+              pullRequests(states: MERGED) {
+                totalCount
+              }
+              issuesclosed: issues(states: CLOSED) {
+                totalCount
+              }
+              issuesopen: issues(states: OPEN) {
+                totalCount
+              }
+              releases {
+                totalCount
+              }
+              primaryLanguage {
+                name
+              }
+            }
+          }
+          pageInfo {
+            endCursor
+          }
+        }
+      }`
+  }
+
+  promises.push(client.request(query))
+}
+const resolves = await Promise.all(promises);
+const repos = resolves.map(repo => repo.search.nodes).flat()
+
+console.log(repos)
+console.log(`Quantidade de repos: ${repos.length}`)
