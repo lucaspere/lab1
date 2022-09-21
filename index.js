06c81456c1c4a11db0e8d2099f263932f199c215
Lucas Pereira <lucasrupestre@hotmail.com>
2022-09-21 13:25:09 -0300

    versao primeira

--- index.js
+++ index.js
@@ -1,31 +1,83 @@
-import { GraphQLClient } from 'graphql-request'
-import { normalizeCsvData, setQuery, writeCSVFile } from './utils.js';
-import { config } from 'dotenv'
+import { GraphQLClient } from "graphql-request";
+import { getPRsQuery, writeCSVFile, getCSV, getReposQuery } from "./utils.js";
+import { config } from "dotenv";
 
-config()
+config();
 
-const client = new GraphQLClient('https://api.github.com/graphql', {
+const client = new GraphQLClient("https://api.github.com/graphql", {
   headers: {
     authorization: `bearer ${process.env.GITHUB_TOKEN}`,
   },
-})
+});
 
-const results = []
+const calculeTempoEmHoras = (date1, date2) => {
+  const closedAt = new Date(date1);
+  const createdAt = new Date(date2);
+  const horas = Math.floor(Math.abs(closedAt - createdAt) / 3.6e6);
 
-for (let i = 0; i < 50; i++) {
-  const after = results.at(i - 1)?.search.pageInfo.endCursor
-  const queryComplement = after && `, after: "${after}"`
-  const data = await client.request(setQuery(queryComplement))
+  return horas;
+};
+const filteredPR = (nodes) => {
+  const data = nodes.filter(
+    (node) =>
+      calculeTempoEmHoras(node.closedAt, node.createdAt) >= 1 &&
+      node.reviews.totalCount >= 1
+  );
 
-  results.push(data)
-}
+  return data;
+};
 
-const nodes = results
-  .map(node => node.search.nodes)
-  .flat()
-  .map(node => normalizeCsvData(node))
+const getRepos = async () => {
+  const data = await client.request(getReposQuery());
+  const csvData = data.search.nodes.map((node) => ({
+    name: node.name,
+    owner: node.owner.login,
+    url: node.url,
+  }));
 
-console.log(nodes);
-console.log("\n" + nodes.length);
+  await writeCSVFile(csvData, "repositorios");
+};
 
-await writeCSVFile(nodes)
+const getPRs = async (repo) => {
+  let after = "";
+  let hasNextPage = false;
+  const results = [];
+  let count = 100;
+  do {
+    const queryComplement = after && `, after: "${after}"`;
+    const data = await client.request(getPRsQuery(repo, queryComplement));
+    console.log(`contador de ${repo.name}: ${count}`);
+    const prs = filteredPR(data.repository.pullRequests.nodes);
+    if (prs.length) {
+      results.push(getMetrics(prs));
+    }
+
+    after = data.repository.pullRequests.pageInfo?.endCursor;
+    hasNextPage = data.repository.pullRequests.pageInfo?.hasNextPage;
+  } while (hasNextPage);
+
+  return results.flat();
+};
+
+const getMetrics = (data) => {
+  return data.map((d) => ({
+    repositorio: d.repository.name,
+    url: d.url,
+    state: d.state,
+    tamanho: d.changedFiles,
+    tempoDeAnalise: calculeTempoEmHoras(d.closedAt, d.createdAt),
+    descricao: d.bodyText.length,
+    interacoes:
+      d.comments.nodes.reduce((sum, node) => (sum += node?.id ? 1 : 0), 0) +
+      d.reviewThreads.totalCount,
+  }));
+};
+
+await getRepos();
+getCSV("./repositorios", async (repos) => {
+  for (let i = 0; i < repos.length; i++) {
+    console.log(`analisando ${repos[i].name}`);
+    const results = await getPRs(repos[i]);
+    await writeCSVFile(results, "teste2");
+  }
+});
--- utils.js
+++ utils.js
@@ -1,67 +1,102 @@
-import { gql } from 'graphql-request'
-import ObjectsToCsv from 'objects-to-csv'
-import fs from 'node:fs'
-import {parse} from 'csv-parse'
+import { gql } from "graphql-request";
+import ObjectsToCsv from "objects-to-csv";
+import fs, { read } from "node:fs";
+import { parse } from "csv-parse";
 
 export const normalizeCsvData = (node) => {
-    let node2 = {}
-    Object.keys(node).map(key => {
-        if (typeof node[key] === 'object') {
-            console.log(node[key])
-            const chave = node[key] ? Object.keys(node[key])[0] : null
-            node2[key] = chave ? node[key][chave] : node[key]
-        } else {
-            node2[key] = node[key]
+  let node2 = {};
+  Object.keys(node).map((key) => {
+    if (typeof node[key] === "object") {
+      console.log(node[key]);
+      const chave = node[key] ? Object.keys(node[key])[0] : null;
+      node2[key] = chave ? node[key][chave] : node[key];
+    } else {
+      node2[key] = node[key];
+    }
+  });
+
+  return node2;
+};
+
+export const getPRsQuery = (repo, complement = "") => gql`{
+  repository(name: "${repo.name}", owner: "${repo.owner}") {
+    pullRequests(states:[MERGED, CLOSED], first: 100 ${complement}) {
+      nodes {
+        state
+        url
+        createdAt
+        closedAt
+        mergedAt
+        reviewThreads {
+          totalCount
         }
-    })
-
-    return node2
+        reviews {
+          totalCount
+        }
+        changedFiles
+        bodyText
+        repository {
+          name
+        }
+        comments(first: 100) {
+          totalCount
+          nodes {
+            author {
+              ... on User {
+                id
+              }
+            }
+          }
+        }
+      }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+    }
+  }
 }
+  `;
 
-export const setQuery = (complement = "") => gql`{
-    search(query: "stars:>100, language:java", type: REPOSITORY, first: 20 ${complement}) {
+export const getReposQuery = () => gql`
+  {
+    search(query: "stars:>100", type: REPOSITORY, first: 100) {
       nodes {
         ... on Repository {
           name
-          createdAt
-          updatedAt
-          pullRequests(states: MERGED) {
-            totalCount
-          }
-          releases {
-            totalCount
-          }
-          primaryLanguage {
-            name
-          }
-          issuesclosed: issues(states: CLOSED) {
-            totalCount
-          }
-          issuesopen: issues(states: OPEN) {
-            totalCount
+          owner {
+            id
+            login
           }
           url
-          stargazerCount
         }
-      }
-      pageInfo {
-        endCursor
       }
     }
   }
-  `
+`;
 
-export const writeCSVFile = async (data, filename = 'dados') => {
-    const csv = new ObjectsToCsv(data);
+export const writeCSVFile = async (data, filename = "dados") => {
+  const csv = new ObjectsToCsv(data);
 
-    // Save to file:
-    await csv.toDisk(`./${filename}.csv`);
+  // Save to file:
+  await csv.toDisk(`./${filename}.csv`, { append: true });
 };
 
-export const getCSV = (path) => (
-  fs.createReadStream(path).pipe(parse({
-    delimiter: ",",
-    columns: true,
-    ltrim: true,
-    }))
-)
+export const getCSV = (path, cb) => {
+  const rows = [];
+  const reader = fs.createReadStream(path + ".csv").pipe(
+    parse({
+      delimiter: ",",
+      columns: true,
+      ltrim: true,
+    })
+  );
+
+  reader.on("data", (data) => {
+    rows.push(data);
+  });
+
+  reader.on("end", () => {
+    cb(rows);
+  });
+};
