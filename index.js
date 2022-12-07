df93e23c4134e67d014b01e7fb9eecc41f1e8459
lucaspere <lucasrupestre@hotmail.com>
2022-12-07 15:19:45 -0300

    finish sprint2

--- index.js
+++ index.js
@@ -1,31 +1,81 @@
-import { GraphQLClient } from 'graphql-request'
-import { normalizeCsvData, setQuery, writeCSVFile } from './utils.js';
-import { config } from 'dotenv'
+import { GraphQLClient } from "graphql-request";
+import { writeCSVFile } from "./utils.js";
+import { config } from "dotenv";
+import fetch from "node-fetch";
 
-config()
+config();
 
-const client = new GraphQLClient('https://api.github.com/graphql', {
-  headers: {
-    authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+const client = new GraphQLClient("https://rickandmortyapi.com/graphql");
+
+const queries = [
+  {
+    description: "Find character where id is 151",
+    rest: "https://rickandmortyapi.com/api/character/151",
+    graphql:
+      'query{characters(filter:{name:"Gwendolyn"}){results{id name status species type gender origin{name} location{name} image episode{name} created}}}',
   },
-})
+  {
+    description: "List episodios",
+    rest: "https://rickandmortyapi.com/api/episode",
+    graphql:
+      "query{episodes{info{count pages next prev}results{id name air_date episode created characters{id name}}}}",
+  },
+  {
+    description: "Get episodio 10 and 28",
+    rest: "https://rickandmortyapi.com/api/episode/10,28",
+    graphql:
+      "query{episodesByIds(ids:[10,28]){id name air_date episode created characters{id name}}}",
+  },
+  {
+    description:
+      "List episodios where character name is rick and status is alive",
+    rest: "https://rickandmortyapi.com/api/character/?name=rick&status=alive",
+    graphql:
+      'query{characters(filter:{name:"rick",status:"alive"}){results{id name status species type gender origin{name} location{name} image episode{id} created}}}',
+  },
+  {
+    description: "List Locations",
+    rest: "https://rickandmortyapi.com/api/location",
+    graphql:
+      "query{locations{info{count pages next prev}results{id name type dimension residents{id} created}}}",
+  },
+];
 
-const results = []
+const results = [];
 
-for (let i = 0; i < 50; i++) {
-  const after = results.at(i - 1)?.search.pageInfo.endCursor
-  const queryComplement = after && `, after: "${after}"`
-  const data = await client.request(setQuery(queryComplement))
-
-  results.push(data)
+for (let i = 0; i < queries.length; i++) {
+  const url = queries[i].rest;
+  const startTime = new Date();
+  const res = await fetch(url);
+  const endTime = new Date();
+  const body = await res.json();
+  const payloadLen = JSON.stringify(body).length;
+  results.push({
+    type: "REST",
+    description: queries[i].description,
+    payloadLen,
+    timeElaped: endTime - startTime,
+    url,
+    query: null,
+  });
 }
 
-const nodes = results
-  .map(node => node.search.nodes)
-  .flat()
-  .map(node => normalizeCsvData(node))
+for (let i = 0; i < queries.length; i++) {
+  const query = queries[i].graphql;
+  const startTime = new Date();
+  const res = await client.request(query);
+  const endTime = new Date();
+  const payloadLen = JSON.stringify(res).length;
 
-console.log(nodes);
-console.log("\n" + nodes.length);
+  results.push({
+    type: "GRAPHQL",
+    description: queries[i].description,
+    payloadLen,
+    timeElaped: endTime - startTime,
+    url: null,
+    query,
+  });
+}
 
-await writeCSVFile(nodes)
+console.log(results);
+await writeCSVFile(results, "rick_morty_data");
